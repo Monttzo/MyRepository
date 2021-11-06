e8a190b3a3f1ed0474d5a7a1ff56740264183d11
Monttzo <dpulido59@gmail.com>
2021-11-05 23:25:41 -0500

    Correction

--- src/views/NursingIsailo.vue
+++ src/views/NursingIsailo.vue
@@ -6,7 +6,7 @@
         <p>This project was created during the year of 2020, and was developed in Html, css3 ,Php and Javascript, 
             additionaly, was created a database in Mysql, using pattern MVC.</p>
         <p>Only have one funcionally model, it is Users. We can login and do the CRUD with the users,
-            in this project exists 3 type of users: Medic, Nurse and Pacient</p>
+            in this project exists 3 type of users: Medic, Nurse and Patient</p>
         <p>This project have a drop-down menu designed in Javascript with DOM</p>
         <img src="../../public/NursingIsailoLogin.png" class="imgRepo">
         <img src="../../public/NursingIsailoHome.png" class="imgRepo">
